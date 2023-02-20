90cb2347d9ffc9b58485231d760d97dd56a33094
Gausroth <66494221+Gausroth@users.noreply.github.com>
2023-02-20 14:58:03 -0600

    Updated Dark Mode & Text
    Moved all images to their own folder.
    Updated Dark Mode to change all buttons background and text color.

--- script.js
+++ script.js
@@ -1,6 +1,39 @@
+let isDarkMode = false;
+
 function DarkMode() {
-    var element = document.body;
-    element.classList.toggle("dark-mode");
+    var darkModeBtn = document.getElementById("darkModeBtn");
+    var buttons = document.getElementsByName("btn");
+    var top = document.getElementsByName("top");
+    isDarkMode = !isDarkMode;
+
+    if (isDarkMode) {
+        console.log(isDarkMode);
+        document.body.style.backgroundColor = "rgb(56,56,56)";
+        document.body.style.color = "#C0C0C0";
+        darkModeBtn.style.backgroundColor = "#C0C0C0";
+        darkModeBtn.style.color = "rgb(56,56,56)";
+        for (var i = 0; i < buttons.length; i++) {
+            buttons[i].style.backgroundColor = "#C0C0C0";
+            buttons[i].style.color = "rgb(56,56,56)";
+        }
+        for (var i = 0; i < top.length; i++) {
+            top[i].style.color = "#C0C0C0";
+        }
+    }
+    else {
+        console.log(isDarkMode);
+        document.body.style.backgroundColor = "#C0C0C0";
+        document.body.style.color = "rgb(56,56,56)";
+        darkModeBtn.style.backgroundColor = "rgb(56,56,56)";
+        darkModeBtn.style.color = "#C0C0C0";
+        for (var i = 0; i < buttons.length; i++) {
+            buttons[i].style.backgroundColor = "rgb(56,56,56)";
+            buttons[i].style.color = "#C0C0C0";
+        }
+        for (var i = 0; i < top.length; i++) {
+            top[i].style.color = "rgb(56,56,56)";
+        }
+    }
 }
 
 window.onload = DarkMode();
